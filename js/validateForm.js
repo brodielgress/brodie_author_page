7de26aad275ae66dad58029d0e1846c083ca7c32
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-03-24 23:42:39 -0400

    Got the form validation for the name to work, with mentor Jacob's help.

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,10 +1,8 @@
 
 // Validates the form inputs
 
-function validateName(event) {
-    event.preventDefault();
+function validateName() {
     let formval = document.getElementById("firstname").value;
-    debugger;
     if (formval == "") {
         alert("Please fill out your name.");
         return false;
